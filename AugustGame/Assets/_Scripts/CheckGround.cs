f92bccebb74338849ad58085d4f6c52259ae007e
Unknown <ki575@nyu.edu>
2018-07-28 15:58:50 +0800

    Move, Jump finished.

---
+++ AugustGame/Assets/_Scripts/CheckGround.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+	{
+        print("collision detected");
+        if(collision.gameObject.CompareTag("ground"))	
+        {
+            GetComponentInParent<PlayerControl>().isGrounded = true;
+        }
+	}
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            GetComponentInParent<PlayerControl>().isGrounded = false;
+        }
+    }
+}
---
+++ AugustGame/Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    Rigidbody2D rbPlayer;
+    Vector2 move;
+    public float jumpForce;
+    public float moveSpeed = 1f;
+    //public LayerMask groundLayers;
+    //public Transform checkGround;
+    public bool isGrounded;
+	// Use this for initialization
+	void Start () {
+        rbPlayer = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        move = new Vector2(Input.GetAxis("Horizontal")* moveSpeed, rbPlayer.velocity.y);
+        rbPlayer.velocity = move;
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rbPlayer.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+	}
+}
