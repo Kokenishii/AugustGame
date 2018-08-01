6bc941ebea08272fba8b53a796b0ec0d913371e6
Unknown <ki575@nyu.edu>
2018-08-01 23:23:41 +0800

    UI System Added

---
+++ AugustGame/Assets/UIManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+    public int uiIndex;
+    public GameObject thisUI;
+    bool isTriggered = false;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        if(isTriggered)
+        {
+            
+            if (
+               Input.GetKey(KeyCode.Space)
+               && thisUI != null)
+            {
+                print("UIManager:Triggered");
+                thisUI.SetActive(true);
+                //StartCoroutine(UIdead());
+
+            }
+        }
+           
+	}
+
+private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            isTriggered = true;
+
+        }
+     
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isTriggered = false;
+            StartCoroutine(UIdead());
+        }
+    }
+
+
+
+
+    IEnumerator UIdead()
+    {
+        yield return new WaitForSeconds(1f);
+        thisUI.SetActive(false);
+    }
+
+}
