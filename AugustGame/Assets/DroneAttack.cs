0d60e4ea3fc78f321ee34fd3b9c64bcb06cbd6ee
Unknown <ki575@nyu.edu>
2018-09-08 15:59:15 -0400

    Final Demo

---
+++ AugustGame/Assets/DroneAttack.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DroneAttack : MonoBehaviour {
+    public float droneAttackSeconds = 0.2f;
+    public bool isAttacking = false;
+    public bool isWorking = true;
+    public GameObject triangle;
+    SpriteRenderer mySprite;
+	// Use this for initialization
+	void Start () {
+        isAttacking = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(isAttacking)
+        {
+          
+            Scene thisScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(thisScene.name);
+        }
+        if(!isWorking)
+        {
+            StartCoroutine(BackToWork());
+        }
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")&&!Input.GetKey(KeyCode.E))
+        {
+            triangle.SetActive(true);
+            StartCoroutine(ToAttack());
+        }
+    }
+
+    IEnumerator ToAttack()
+    {
+        yield return new WaitForSeconds(droneAttackSeconds);
+        isAttacking = true;
+
+    }
+
+    IEnumerator BackToWork()
+    {
+        yield return new WaitForSeconds(3);
+        isWorking = true;
+
+    }
+}
---
+++ AugustGame/Assets/UIAuto.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAuto : MonoBehaviour {
+
+	// Use this for initialization
+    public int uiIndex;
+    public GameObject thisUI;
+    bool isTriggered = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isTriggered)
+        {
+
+           // if (
+              // Input.GetKey(KeyCode.E)
+              // && thisUI != null)
+           // {
+                //print("UIManager:Triggered");
+                thisUI.SetActive(true);
+                //StartCoroutine(UIdead());
+
+            //}
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isTriggered = true;
+
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isTriggered = false;
+            StartCoroutine(UIdead());
+        }
+    }
+
+
+
+
+    IEnumerator UIdead()
+    {
+        yield return new WaitForSeconds(1f);
+        thisUI.SetActive(false);
+    }
+}
